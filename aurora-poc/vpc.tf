754aecf8610b45fcd79ac0dbd09d46efff5b7bff
melophe <sunlavins@gmail.com>
2026-03-22 15:47:20 +0900

    add private route table and S3 VPC endpoint for Glue access

--- aurora-poc/vpc.tf
+++ aurora-poc/vpc.tf
@@ -83,3 +83,22 @@ resource "aws_route_table_association" "public_c" {
   subnet_id      = aws_subnet.public_c.id
   route_table_id = aws_route_table.public.id
 }
+
+# プライベートルートテーブル
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "aurora-poc-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_c" {
+  subnet_id      = aws_subnet.private_c.id
+  route_table_id = aws_route_table.private.id
+}
---
+++ aurora-poc/vpc_endpoints.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [aws_route_table.private.id]
+
+  tags = {
+    Name = "aurora-poc-s3-endpoint"
+  }
+}
